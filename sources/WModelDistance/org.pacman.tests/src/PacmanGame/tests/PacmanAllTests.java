bb8650156f042122405cf74ae2347f05ed6a24ee
syriani <syriani@iro.umontreal.ca>
2018-08-02 09:15:15 +0200

    removed test project

---
+++ sources/WModelDistance/org.pacman.tests/src/PacmanGame/tests/PacmanAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package PacmanGame.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Pacman</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PacmanAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new PacmanAllTests("Pacman Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PacmanAllTests(String name) {
+		super(name);
+	}
+
+} //PacmanAllTests
